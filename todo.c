43d737fe4db4fb38ca0c93a833a390b37c4886a5
Monty Behar-Sheehan <monty081@outlook.com>
2024-06-11 00:23:59 +0100

    Added functionality

--- todo.c
+++ todo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 
 void add_task();
 void remove_task();
@@ -19,14 +21,22 @@ int main() {
 	while(1) {
 		display_menu();
 		printf("Enter your choice: ");
-		scanf("%d", &choice);
-		getchar();
-		printf("Entered: %c", choice);
+		scanf("%d", &choice); //Take the integer in stream and store in address of variable choice
+		getchar(); //Consume the newline character | don't interrupt stream
 		switch (choice) {
 			case 1:
 				add_task();
 				break;
-				
+			case 2:
+				remove_task();
+				break;
+			case 3:
+				view_task();
+				break;
+			case 5:
+				exit(0);
+			default:
+				printf("Invalid choice, try again!\n");
 		}
 	}
 }
@@ -49,6 +59,27 @@ void add_task() {
 		printf("Task list is too full!");
 	}
 }
+//Need to add validation to input, check if valid task number
+void remove_task() {
+	int task_num;
+	printf("Enter task number to be removed: ");
+	scanf("%d", &task_num);
+	getchar();
 
+	if (task_num > 0 && task_num <= task_count) {
+		for (int i = task_num - 1; i < task_count - 1; i++) {
+			strcpy(tasks[i], tasks[i+1]);
+		}
+		task_count--;
+		printf("Task Removed \n");
+	} else {
+		printf("Invalied task number. \n");
+	}
 
+}
 
+void view_task() {
+	for (int i = 0; i < task_count; i++) {
+		printf("%d %s\n", i+1, tasks[i]);
+	}
+}
